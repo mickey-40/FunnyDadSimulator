8b8862efd4c165f0b877b62f03b95911689a3232
Mickey Arnold <mickeyarnold@Mickeys-MBP.lan>
2022-02-01 20:31:19 -0600

    Setup html file and started js file

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const gernerateQuote = function (){
+    const quotes = [
+        {
+            quote: “HAVING CHILDREN IS LIKE LIVING IN A FRAT HOUSE. NOBODY SLEEPS, EVERYTHING’S BROKEN AND THERE’S A LOT OF THROWING UP.”
+            author: "RAY ROMANO"
+        },
+        {
+            quote: “MEN SHOULD ALWAYS CHANGE DIAPERS. IT’S A VERY REWARDING EXPERIENCE. IT’S MENTALLY CLEANSING. IT’S LIKE WASHING DISHES, BUT IMAGINE IF THE DISHES WERE YOUR KIDS, SO YOU REALLY LOVE THE DISHES.”
+            author: "CHRIS MARTIN"
+        },
+        {
+            quote: “BUYING YOUR KID A GOLDFISH IS A GREAT WAY TO TEACH THEM ABOUT RESPONSIBILITY FOR 24—36 HOURS.” 
+            author: "CONAN O’BRIEN"
+        },
+        {
+            quote: “A FATHER CARRIES PICTURES WHERE HIS MONEY USED TO BE.”
+            author: "STEVE MARTIN"
+        }
+    ]
+}
